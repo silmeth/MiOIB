8e74e4ffeba18a6c4ae484cfecfd67a6d3b846b4
Jacek Jankowski <spock.sheldon@gmail.com>
2014-10-20 12:43:56 +0200

    Sputnik test.

---
+++ src/sputnik_test.cpp
@@ -0,0 +1,23 @@
+/*
+ * sputnik_test.cpp
+ *
+ *  Created on: Oct 20, 2014
+ *      Author: jacek
+ */
+
+#ifdef SPUTNIK_TEST // define SPUTNIK_TEST before compile to enable this file
+#include <iostream>
+
+#include "../inc/algorithms.h"
+#include "../inc/instances.h"
+#include "../inc/measures.h"
+#include "../inc/operations.h"
+
+int main() {
+}
+
+
+#endif // SPUTNIK_TEST
+
+
+
