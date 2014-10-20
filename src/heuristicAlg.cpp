a820429bb2cf29c893d37f3b7fe299041ddcd59e
silmeth <sth.jaworski@gmail.com>
2014-10-20 15:57:35 +0200

    created heuristic algorithm

--- src/heuristicAlg.cpp
+++ src/heuristicAlg.cpp
@@ -5,6 +5,53 @@
  *      Author: jacek
  */
 
+#include "../inc/heuristicAlg.h"
 
+bool* isUsed;
 
+int* genSolution(int* solution) {
+    isUsed = new bool[sz];
+    for(unsigned int i = 0; i < sz; i++)
+        isUsed[i] = false;
 
+    for(unsigned int k = 0; k < sz; k++) {
+        bool first = true;
+        int iAMax = 0;
+        int jAMax = 0;
+        int AMax = 0;
+        int iBMin = 0;
+        int jBMin = 0;
+        int BMin = 0;
+        for(unsigned int i = 0; i < sz; i++) {
+            for(unsigned int j = 0; j < sz; j++) {
+                if(!isUsed[i] && !isUsed[j] && i != j) {
+                    if(first) {
+                        AMax = A[i][j];
+                        iAMax = i;
+                        jAMax = j;
+                        BMin = B[i][j];
+                        iBMin = i;
+                        jBMin = j;
+                        first = false;
+                    } else {
+                        if(A[i][j] > AMax) {
+                            iAMax = i;
+                            jAMax = j;
+                        }
+                        if(B[i][j] < BMin) {
+                            iBMin = i;
+                            jBMin = j;
+                        }
+                    }
+                }
+            }
+        }
+        solution[iAMax] = iBMin;
+        solution[jBMax] = jBMin;
+
+        isUsed[i] = true;
+        isUsed[j] = true;
+    }
+    delete[] isUsed;
+    return solution;
+}
