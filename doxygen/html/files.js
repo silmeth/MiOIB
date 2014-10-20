3e192d1159e003b70d8f486c325f1780fc340b4f
Jacek Jankowski <spock.sheldon@gmail.com>
2014-10-20 12:53:51 +0200

    Remove doxygen.

--- doxygen/html/files.js
+++
@@ -1,7 +0,0 @@
-var files =
-[
-    [ "algorithms.h", "algorithms_8h_source.html", null ],
-    [ "instances.h", "instances_8h_source.html", null ],
-    [ "measures.h", "measures_8h_source.html", null ],
-    [ "operations.h", "operations_8h_source.html", null ]
-];
